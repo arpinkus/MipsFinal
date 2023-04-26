6d63b0c0ff5ebabf595bca987562dc31b475826a
abbypin <abby@techcleanup.com>
2023-04-26 14:51:33 -0700

    Create mergeSort.asm
    Abigail Pinkus Created mergeSort.asm file and added beginning layout code.

---
+++ mergeSort.asm
@@ -0,0 +1,16 @@
+# Name: Abigail Pinkus, 
+# Date: May 12, 2023
+# Program: Merge Sort: Final
+# main, Loop, Exit
+
+.data
+# Arrays to Sort #
+array: .word 4,2,5,6,1,3
+.text
+main:
+
+Exit:
+	# Exit Program #
+	li $v0, 10
+	syscall
+#end program
